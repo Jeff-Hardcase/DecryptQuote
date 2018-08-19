e6807792cb616101cde68bab5e519a7c299cd824
Jeff <Jeff@DARTHVADER>
2018-08-18 21:56:44 -0500

    Update

---
+++ DecryptQuote/Controllers/SolveController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DecryptQuote.Controllers
+{
+    public class SolveController : Controller
+    {
+        // GET: Solve
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Grid(string encryptedQuote)
+        {
+            var gridModel = new DecryptQuote.Models.QuoteModel();
+
+            gridModel.OriginalText = encryptedQuote;
+            gridModel.SplitText = encryptedQuote.Split('');
+
+
+            return PartialView(gridModel);
+        }
+    }
+}
---
+++ DecryptQuote/Models/QuoteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DecryptQuote.Models
+{
+    public class QuoteModel
+    {
+        public string OriginalText { get; set; }
+        public string[] SplitText { get; set; }
+    }
+}
